5170a6c8d9a125e4f3ea1f05729a2652e9aead0e
Nada Amin <namin@seas.harvard.edu>
2023-08-13 17:08:05 +0200

    need to add constants

--- test/dsl_test.scala
+++ test/dsl_test.scala
@@ -16,4 +16,13 @@ class ArithDslTesting extends AnyFunSuite {
       bottomup(List("x"),
         List(IOEx(List(1), -1), IOEx(List(2), -2))))
   }
+  // TODO: more tests that require constants
+  /*
+   assert ("add", [("input", 0), ("neg", [1])]) == bustle(
+   al, int2, [[1, 2, 3]], [0, 1, 2], llProps, Ms
+   )
+   assert ("if", [("lt", [("input", 0), ("input", 1)]), 1, 0]) == bustle(
+   al, int3, [[1, 2, 3], [3, 1, 2]], [1, 0, 0], llProps
+   )
+   */
 }
