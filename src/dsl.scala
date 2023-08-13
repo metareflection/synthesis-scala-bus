1ca9c025badec3f2d53ca0a5baa5c36330f6b1b5
Nada Amin <namin@seas.harvard.edu>
2023-08-13 16:39:00 +0200

    rename op to opcode in prep for BUS integration

--- src/dsl.scala
+++ src/dsl.scala
@@ -1,10 +1,10 @@
 trait Dsl {
-  val ops: List[String]
+  val opcodes: List[String]
   val types: List[String]
-  lazy val opCache: Set[String] = ops.toSet
+  lazy val opcodeCache: Set[String] = opcodes.toSet
 
-  def execute(op: String, args: List[Any]): Any
-  def types(op: String): (String, List[String])
+  def execute(opcode: String, args: List[Any]): Any
+  def types(opcode: String): (String, List[String])
   def inferTypes(v: Any): String
   // TODO: extractConstants
 }
@@ -19,9 +19,9 @@ trait DslBottomUpSearch extends bus.BottomUpSearch {
 }
 
 trait ArithDsl extends Dsl {
-  override val ops = List("add", "mul", "div", "neg", "lt", "if")
+  override val opcodes = List("add", "mul", "div", "neg", "lt", "if")
   override val types = List("int", "bool")
-  override def execute(op: String, args: List[Any]): Any = op match {
+  override def execute(opcode: String, args: List[Any]): Any = opcode match {
     case "add" => args(0).asInstanceOf[Int] + args(1).asInstanceOf[Int]
     case "mul" => args(0).asInstanceOf[Int] * args(1).asInstanceOf[Int]
     case "div" => args(0).asInstanceOf[Int] / args(1).asInstanceOf[Int]
@@ -29,14 +29,14 @@ trait ArithDsl extends Dsl {
     case "lt" => args(0).asInstanceOf[Int] < args(1).asInstanceOf[Int]
     case "if" => if (args(0).asInstanceOf[Boolean]) args(1) else args(2)
   }
-  override def types(op: String): (String, List[String]) =
-    if (Set("neg").contains(op))
+  override def types(opcode: String): (String, List[String]) =
+    if (Set("neg").contains(opcode))
         ("int", List("int"))
-    else if (Set("add", "mul", "div").contains(op))
+    else if (Set("add", "mul", "div").contains(opcode))
         ("int", List("int", "int"))
-    else if (Set("lt").contains(op))
+    else if (Set("lt").contains(opcode))
         ("bool", List("int", "int"))
-    else if (Set("if").contains(op))
+    else if (Set("if").contains(opcode))
         ("int", List("bool", "int", "int"))
     else assert(false)
   override def inferTypes(v: Any): String =
